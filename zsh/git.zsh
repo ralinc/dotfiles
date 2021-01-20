e1148c130fbb808d2c59d9ce83285f49be7995a3
Ralin Chimev <ralin.chimev@gmail.com>
2021-01-20 17:16:45 +0200

    Add structure.sql shell helpers

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -9,3 +9,12 @@ function gres() {
   git reset --hard origin/$1
   git checkout -
 }
+
+function csql() {
+  git add db/structure.sql
+  git commit -m "structure.sql"
+}
+
+function rsql() {
+  git checkout master -- db/structure.sql
+}
