01b977fd4f027f67b86acae0700aedc54bc80d9d
Ralin Chimev <ralin.chimev@gmail.com>
2021-03-10 15:47:37 +0200

    Clean up unused stuff

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,4 @@
 alias ls="ls -FGh"
 alias sed="sed -E"
-alias rmq="brew services restart rabbitmq"
-alias rds="brew services restart redis"
 alias tdl="tail -f log/development.log"
-alias lim="vim -u ~/.vimrc.light"
 alias ctags="`brew --prefix`/bin/ctags"
-alias prc="hub pr checkout "
