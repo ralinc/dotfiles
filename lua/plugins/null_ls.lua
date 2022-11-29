069a63d78e8b48c311c4d02c8a123e1e6808cb01
Ralin Chimev <ralin.chimev@gmail.com>
2022-11-29 20:55:21 +0200

    nvim: Hacks for Yarn PnP and treesitter bugs :(

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -10,14 +10,12 @@ null_ls.setup({
     null_ls.builtins.diagnostics.rubocop,
     null_ls.builtins.formatting.rubocop,
 
-    null_ls.builtins.diagnostics.eslint.with({
-      command = "yarn",
-      args = { "eslint", "-f", "json", "--stdin", "--stdin-filename", "$FILENAME" },
-    }),
-    null_ls.builtins.diagnostics.stylelint.with({
-      command = "yarn",
-      args = { "stylelint", "--formatter", "json", "--stdin-filename", "$FILENAME" },
-    }),
+    null_ls.builtins.diagnostics.eslint,
+    -- null_ls.builtins.diagnostics.eslint.with({
+    --   command = "yarn",
+    --   args = { "eslint", "-f", "json", "--stdin", "--stdin-filename", "$FILENAME" },
+    -- }),
+
     null_ls.builtins.formatting.prettier.with({
       filetypes = {
         "css",
@@ -33,6 +31,12 @@ null_ls.setup({
         "yaml",
       },
     }),
+
+    null_ls.builtins.diagnostics.stylelint,
+    -- null_ls.builtins.diagnostics.stylelint.with({
+    --   command = "yarn",
+    --   args = { "stylelint", "--formatter", "json", "--stdin-filename", "$FILENAME" },
+    -- }),
 
     null_ls.builtins.formatting.gofmt,
     null_ls.builtins.formatting.goimports,
