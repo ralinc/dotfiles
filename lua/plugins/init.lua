6dbff3ac3e0162be559647e8406a55003fd44b0b
Ralin Chimev <ralin.chimev@gmail.com>
2022-12-19 14:01:22 +0200

    nvim: Replace minpac with packer

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,38 +1,4 @@
-vim.cmd [[
-packadd minpac
-call minpac#init()
-
-call minpac#add('christoomey/vim-tmux-navigator')
-call minpac#add('christoomey/vim-tmux-runner')
-call minpac#add('jose-elias-alvarez/null-ls.nvim')
-call minpac#add('k-takata/minpac', {'type': 'opt'})
-call minpac#add('L3MON4D3/LuaSnip')
-call minpac#add('neovim/nvim-lspconfig')
-call minpac#add('nvim-lua/plenary.nvim')
-call minpac#add('hrsh7th/cmp-buffer')
-call minpac#add('hrsh7th/cmp-nvim-lua')
-call minpac#add('hrsh7th/cmp-nvim-lsp')
-call minpac#add('hrsh7th/cmp-path')
-call minpac#add('hrsh7th/nvim-cmp')
-call minpac#add('kyazdani42/nvim-tree.lua')
-call minpac#add('mileszs/ack.vim')
-call minpac#add('nanotech/jellybeans.vim')
-call minpac#add('nvim-lualine/lualine.nvim')
-call minpac#add('nvim-telescope/telescope.nvim')
-call minpac#add('nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'})
-call minpac#add('nvim-treesitter/nvim-treesitter-textobjects')
-call minpac#add('numToStr/Comment.nvim')
-call minpac#add('saadparwaiz1/cmp_luasnip')
-call minpac#add('slim-template/vim-slim')
-call minpac#add('thoughtbot/vim-rspec')
-call minpac#add('tpope/vim-endwise')
-call minpac#add('tpope/vim-fugitive')
-call minpac#add('tpope/vim-sleuth')
-call minpac#add('tpope/vim-surround')
-
-command! PackUpdate call minpac#update()
-command! PackClean call minpac#clean()
-]]
+require 'plugins/packer'
 
 require('Comment').setup {
   mappings = {
