709f12d21bcd744d954409e53df12b41437766f3
Ralin Chimev <ralin.chimev@gmail.com>
2023-03-23 08:42:25 +0200

    nvim: Add grep in dir keybind

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -41,7 +41,8 @@ nmap('<leader>x', ":split <C-R>=escape(expand(\"%:p:h\"), ' ') . '/'<cr>")
 
 local telescope_builtin = require 'telescope.builtin'
 nmap('<C-p>', telescope_builtin.find_files)
-nmap('<leader>ff', telescope_builtin.live_grep)
+nmap('<leader>f', telescope_builtin.live_grep)
+nmap('<leader>ff', ':Telescope live_grep search_dirs=')
 nmap('<leader>fw', telescope_builtin.grep_string)
 nmap('<leader>fb', telescope_builtin.buffers)
 nmap('<leader>fh', telescope_builtin.help_tags)
