dad926a0e0e5b40dce9923f38598fc8a623de0aa
Ralin Chimev <ralin.chimev@gmail.com>
2024-03-07 09:37:59 +0200

    nvim: auto-detect primary git branch

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,39 +1,43 @@
 function llc() {
-  ls -1 "$@" | wc -l
+    ls -1 "$@" | wc -l
 }
 
 function wip() {
-  git add .
-  git commit -m "WIP $*"
+    git add .
+    git commit -m "WIP $*"
 }
 
 function res() {
-  case $1 in
-    m)
-      BRANCH="master"
-      ;;
-    p)
-      BRANCH="production"
-      ;;
-    s)
-      BRANCH="staging"
-      ;;
-    *)
-      BRANCH=$1
-      ;;
-  esac
+    case $1 in
+        m)
+            if git ls-remote --heads origin main | grep -q 'refs/heads/main$' &> /dev/null; then
+                BRANCH="main"
+            else
+                BRANCH="master"
+            fi
+            ;;
+        p)
+            BRANCH="production"
+            ;;
+        s)
+            BRANCH="staging"
+            ;;
+        *)
+            BRANCH=$1
+            ;;
+    esac
 
-  git fetch
-  git checkout $BRANCH
-  git reset --hard origin/$BRANCH
-  git checkout -
+    git fetch
+    git checkout $BRANCH
+    git reset --hard origin/$BRANCH
+    git checkout -
 }
 
 function csql() {
-  git add db/structure.sql
-  git commit -m "structure.sql"
+    git add db/structure.sql
+    git commit -m "structure.sql"
 }
 
 function rsql() {
-  git checkout master -- db/structure.sql
+    git checkout master -- db/structure.sql
 }
