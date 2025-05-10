a7ef3be11537f3dc83063ace8d0cd88e70b5f5cb
Ralin Chimev <ralin.chimev@gmail.com>
2025-05-10 18:28:35 +0300

    nvim: drop tmux integration

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -76,24 +76,6 @@ require('lazy').setup {
   },
 
   {
-    'christoomey/vim-tmux-navigator',
-    cmd = {
-      'TmuxNavigateLeft',
-      'TmuxNavigateDown',
-      'TmuxNavigateUp',
-      'TmuxNavigateRight',
-      'TmuxNavigatePrevious',
-    },
-    keys = {
-      { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
-      { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
-      { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
-      { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
-      { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
-    },
-  },
-
-  {
     'github/copilot.vim',
     config = function()
       vim.g.copilot_filetypes = { markdown = false }
--- zshrc
+++ zshrc
@@ -2,7 +2,7 @@ BASE="$HOME/.zsh"
 ZSH_THEME=""
 
 export ZSH=$HOME/.oh-my-zsh
-plugins=(git bundler rails tmux)
+plugins=(git bundler rails)
 source $ZSH/oh-my-zsh.sh
 
 for file in $BASE/*.zsh; do
