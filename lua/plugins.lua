63fa4a023c7f2422bd8c110b36fd3a37d8b751bc
Ralin Chimev <ralin.chimev@gmail.com>
2025-09-28 09:05:39 +0300

    nvim: fix xml formatter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,7 +5,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
-      { out, 'WarningMsg' },
+      { out,                            'WarningMsg' },
       { '\nPress any key to exit...' },
     }, true, {})
     vim.fn.getchar()
@@ -109,7 +109,7 @@ require('lazy').setup {
         toml = { 'prettier' },
         typescript = { 'prettier' },
         typescriptreact = { 'prettier' },
-        xml = { 'xmlformat' },
+        xml = { 'xmlformatter' },
         yaml = { 'prettier' },
         zsh = { 'beautysh' },
       },
