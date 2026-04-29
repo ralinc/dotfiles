c36efd311fa6e5ebefb3fd27fa625a93b2a2b3b3
Ralin Chimev <ralin.chimev@gmail.com>
2026-04-29 10:25:49 +0300

    nvim: fix treesitter issue

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,8 +1,7 @@
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.uv.fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out =
-    vim.fn.system { 'git', 'clone', '--filter=blob:none', '--single-branch', '--branch=stable', lazyrepo, lazypath }
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
@@ -176,9 +175,10 @@ require('lazy').setup {
 
   {
     'nvim-treesitter/nvim-treesitter',
+    branch = 'master',
     main = 'nvim-treesitter.configs',
     dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
+      { 'nvim-treesitter/nvim-treesitter-textobjects', branch = 'master' },
       'RRethy/nvim-treesitter-endwise',
     },
     build = ':TSUpdate',
